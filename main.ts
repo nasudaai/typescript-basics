f4116100e495d0a19f49d396dc14f1f65d40b66d
nasuda <nasuda908@gmail.com>
2025-05-24 23:11:38 +0900

    test

---
+++ main.ts
@@ -0,0 +1,6 @@
+//const str:string = 3
+
+const str: string = "3"
+
+console.log("test")
+console.log(str)
---
+++ out/main.js
@@ -0,0 +1,4 @@
+//const str:string = 3
+var str = "3";
+console.log("test");
+console.log(str);
