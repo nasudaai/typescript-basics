bb7fbf7c7f8ea183b0dc2e1efe6a66117d45fe6c
nasuda <nasuda908@gmail.com>
2025-05-26 09:24:40 +0900

    change "main.ts"

--- main.ts
+++ main.ts
@@ -4,3 +4,11 @@ const str: string = "3"
 
 console.log("test")
 console.log(str)
+
+//function main(): number {
+//  return "29";
+//}
+
+function main(): number {
+  return 24;
+}
--- out/main.js
+++ out/main.js
@@ -2,3 +2,9 @@
 var str = "3";
 console.log("test");
 console.log(str);
+//function main(): number {
+//  return "29";
+//}
+function main() {
+    return 24;
+}
