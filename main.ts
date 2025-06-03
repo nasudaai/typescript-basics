51415da9ec93acdaed0f037041fb8deeaedab5e7
nasuda <nasuda908@gmail.com>
2025-06-04 00:12:49 +0900

    learn tsdoc

--- main.ts
+++ main.ts
@@ -1,2 +1,8 @@
+
 let myName: string = "Alice";
 console.log(myName);
+
+//array
+
+const numArr: number[] = [1, 2, 3];
+console.log(numArr);
--- out/main.js
+++ out/main.js
@@ -1,2 +1,5 @@
 var myName = "Alice";
 console.log(myName);
+//array
+var numArr = [1, 2, 3];
+console.log(numArr);
